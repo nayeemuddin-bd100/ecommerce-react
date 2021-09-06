95f56844e0f4282b686d268f66f73b8fee7b328a
Nayeem <nayeemuddin.bd100@gmail.com>
2021-09-06 23:39:31 +0600

    update router setup

--- src/App.js
+++ src/App.js
@@ -3,32 +3,26 @@ import './App.css';
 import Layout from './components/Layout';
 import NotFound from './components/NotFound/NotFound';
 import Blog from './pages/Blog';
+import Cart from './pages/Cart';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
 import Shop from './pages/Shop';
+import WishList from './pages/WishList';
 
 function App() {
     return (
         <Layout className="App">
             <Switch>
-                <Route exact path="/home">
-                    <Home />
-                </Route>
-                <Route exact path="/shop">
-                    <Shop />
-                </Route>
-                <Route exact path="/blog">
-                    <Blog />
-                </Route>
-                <Route exact path="/contact">
-                    <Contact />
-                </Route>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route path="*">
-                    <NotFound />
-                </Route>
+                <Route exact path="/home" component={Home} />
+                <Route exact path="/shop" component={Shop} />
+                <Route exact path="/blog" component={Blog} />
+                <Route exact path="/contact" component={Contact} />
+                <Route exact path="/product/:id" component={ProductDetails} />
+                <Route exact path="/cart" component={Cart} />
+                <Route exact path="/wishlist" component={WishList} />
+                <Route exact path="/" component={Home} />
+                <Route exact path="*" component={NotFound} />
             </Switch>
         </Layout>
     );
