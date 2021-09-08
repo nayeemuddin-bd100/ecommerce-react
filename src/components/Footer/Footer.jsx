b68c3aebd66f520adb1f7ac7ce9bec22e8879b28
Nayeem <nayeemuddin.bd100@gmail.com>
2021-09-08 20:01:57 +0600

    feat: added a border on top

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div className="flex justify-center flex-col items-center font-light text-sm  py-5">
+        <div className="flex justify-center flex-col items-center font-light text-sm  mt-5 border-t border-gray-100 pt-3">
             <h3 className="text-gray-700"> Copyright &copy; 2021. All Rights Reserved</h3>
             <p>
                 {' '}
