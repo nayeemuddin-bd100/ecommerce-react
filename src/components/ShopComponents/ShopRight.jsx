8bf1f17eebf53667ee42ba7a116327a4300da8d3
Nayeem <nayeemuddin.bd100@gmail.com>
2021-10-23 00:39:11 +0600

    feat:fix css

--- src/components/ShopComponents/ShopRight.jsx
+++ src/components/ShopComponents/ShopRight.jsx
@@ -3,7 +3,7 @@ import ProductCard from '../ProductCard/ProductCard';
 
 function ShopRight() {
     return (
-        <div className="grid grid-cols-2 gap-1 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-2 gap-1 sm:grid-cols-3 lg:grid-cols-4">
             <ProductCard />
             <ProductCard />
             <ProductCard />
