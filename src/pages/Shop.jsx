1fb31764a190eb614d4e5eb90c528dbc45e3ea78
Nayeem <nayeemuddin.bd100@gmail.com>
2021-10-23 00:40:54 +0600

    added shop component

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ShopLeft from '../components/ShopComponents/ShopLeft';
 import ShopRight from '../components/ShopComponents/ShopRight';
+import ShopTop from '../components/ShopComponents/ShopTop';
 
 function Shop() {
     return (
         <div>
-            <div className="grid grid-cols-1 sm:grid-cols-4 mx-4 font-osygen">
-                <div className="hidden sm:block">
+            <div className="grid grid-cols-1 md:grid-cols-4 mx-4 font-osygen">
+                <div className="md:hidden mt-3 mx-3 sm:mx-16">
+                    <ShopTop />
+                </div>
+                <div className="hidden md:block">
                     <ShopLeft />
                 </div>
                 <div className="sm:col-span-3  px-4 mt-5">
